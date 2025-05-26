b9676a2c0d181328e08b7fd9ecbee7d6b20caeca
Monish Vassantrai <m.vassantrai@gmail.com>
2025-05-26 18:52:47 +0100

    react hooks - pizza of the day

--- complete-intro-to-react-v9/padre-ginos/src/App.jsx
+++ complete-intro-to-react-v9/padre-ginos/src/App.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay";
 
 const App = () => {
   return (
     <div>
-      <h1>Padre Gino's - Order Now</h1>
+      <h1 className="logo">Padre Gino's - Order Now</h1>
       <Order />
+      <PizzaOfTheDay />
     </div>
   );
 };
